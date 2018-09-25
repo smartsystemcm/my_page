91ee38011127f21d782447ba939938849572ebfd
guilhermecd <guizao.seri@gmail.com>
2018-09-25 09:01:13 -0300

    new day

--- functions_onclick.js
+++ functions_onclick.js
@@ -3,21 +3,15 @@ function button_big_gateway(_this) {
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php";
 	url[1]="https://smartsystemcm.github.io/my_page/";
 
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-			if(_this.value == "ABERTO"){
-			  	_this.style.backgroundColor = "#77878A";
-			  	_this.value = "FECHADO";
-			}
-			else{
-		 		_this.value = "ABERTO";
-		  		_this.style.backgroundColor = "#195B6A";
-			}
-	    }
-	};
-	xhttp.open("GET", "writefile.php?", true);
-	xhttp.send("data=Client:4");
+	if(_this.value == "ABERTO"){
+	  	_this.style.backgroundColor = "#77878A";
+	  	_this.value = "FECHADO";
+	}
+	else{
+		_this.value = "ABERTO";
+		_this.style.backgroundColor = "#195B6A";
+	}
+	$.get("writefile.php", { data:"Client:4"});    
 }
 
 function button_small_gateway(_this) {
