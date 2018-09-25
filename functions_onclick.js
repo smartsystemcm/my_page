e5c0009ebbca8d66683e9bc12f7af32fac90db70
guilhermecd <guizao.seri@gmail.com>
2018-09-24 23:19:46 -0300

    first commit

--- functions_onclick.js
+++ functions_onclick.js
@@ -1,6 +1,10 @@
 function button_big_gateway(_this) {
 	var url=new Array();
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php?data=Client:4";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url[0], false ); // false for synchronous request
+    xmlHttp.send("Client:4");
+
 
 	location.href = url[0];
 	if(_this.value == "ABERTO"){
@@ -11,6 +15,9 @@ function button_big_gateway(_this) {
  		_this.value = "ABERTO";
   		_this.style.backgroundColor = "#195B6A";
 	}
+	
+    return xmlHttp.responseText;
+
 }
 
 function button_small_gateway(_this) {
