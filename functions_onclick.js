6136358d76aa9a8137f139a61da6ef0632fbb2a7
guilhermecd <guizao.seri@gmail.com>
2018-09-25 00:18:34 -0300

    second commit

--- functions_onclick.js
+++ functions_onclick.js
@@ -2,8 +2,10 @@ function button_big_gateway(_this) {
 	var url=new Array();
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php/data=Client:4";
 	url[1]="https://smartsystemcm.github.io/my_page/";
-    $.get( "writefile.php", { "data": "Client:4"} );
-    console.log("ok");
+    $.get( "writefile.php", { "data": "Client:4"} )
+     	.done(function( data ) {
+    	alert( "Data Loaded: " + data );
+  	});
 	if(_this.value == "ABERTO"){
 	  	_this.style.backgroundColor = "#77878A";
 	  	_this.value = "FECHADO";
@@ -22,7 +24,10 @@ function button_small_gateway(_this) {
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php?data=Client:5";
 	url[1]="https://smartsystemcm.github.io/my_page/";
 
-    $.get( "writefile.php", { "data": "Client:5"} );
+    $.get( "writefile.php", { "data": "Client:5"} )
+     	.done(function( data ) {
+    	alert( "Data Loaded: " + data );
+  	});
 	if(_this.value == "ABERTO"){
 	  	_this.style.backgroundColor = "#77878A";
 	  	_this.value = "FECHADO";
