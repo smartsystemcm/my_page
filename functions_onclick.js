1a1935552691b595627687be1de51787237380ae
guilhermecd <guizao.seri@gmail.com>
2018-09-25 00:32:51 -0300

    second commit

--- functions_onclick.js
+++ functions_onclick.js
@@ -2,20 +2,25 @@ function button_big_gateway(_this) {
 	var url=new Array();
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php/data=Client:4";
 	url[1]="https://smartsystemcm.github.io/my_page/";
-    $.get( "writefile.php", { "data": "Client:4"} )
-     	.done(function( data ) {
-    	alert( "Data Loaded: " + data );
-  	});
-	if(_this.value == "ABERTO"){
-	  	_this.style.backgroundColor = "#77878A";
-	  	_this.value = "FECHADO";
-	}
-	else{
- 		_this.value = "ABERTO";
-  		_this.style.backgroundColor = "#195B6A";
-	}
 
-	location.href = url[1];
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+			if(_this.value == "ABERTO"){
+			  	_this.style.backgroundColor = "#77878A";
+			  	_this.value = "FECHADO";
+			}
+			else{
+		 		_this.value = "ABERTO";
+		  		_this.style.backgroundColor = "#195B6A";
+			}
+
+			location.href = url[1];
+	    }
+	};
+	xhttp.open("GET", "writefile.php", true);
+	xhttp.send("data=Client:5");
+
 
 }
 
