8c986c5cacd9c32833bd446208465d2f28c7a0d8
guilhermecd <guizao.seri@gmail.com>
2018-09-25 09:14:38 -0300

    new day

--- functions_onclick.js
+++ functions_onclick.js
@@ -11,7 +11,7 @@ function button_big_gateway(_this) {
 		_this.value = "ABERTO";
 		_this.style.backgroundColor = "#195B6A";
 	}
-	$.get("writefile.php", {"data=Client:4"});    
+	$.get("writefile.php", {data: "data=Client:4"});    
 	
 }
 
@@ -19,11 +19,9 @@ function button_small_gateway(_this) {
 	var url=new Array();
 	url[0]="https://smartsystemcm.github.io/my_page/writefile.php?data=Client:5";
 	url[1]="https://smartsystemcm.github.io/my_page/";
-
-    $.get( "writefile.php", { "data": "Client:5"} )
-     	.done(function( data ) {
-    	alert( "Data Loaded: " + data );
-  	});
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url[0], false ); // false for synchronous request
+    xmlHttp.send( null );
 	if(_this.value == "ABERTO"){
 	  	_this.style.backgroundColor = "#77878A";
 	  	_this.value = "FECHADO";
@@ -32,6 +30,7 @@ function button_small_gateway(_this) {
  		_this.value = "ABERTO";
   		_this.style.backgroundColor = "#195B6A";
 	}
+	return xmlHttp.responseText;
 	location.href = url[1];
 }
 
