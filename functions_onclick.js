d72a9b28335116672800c4437546855b17a40c8b
guilhermecd <guizao.seri@gmail.com>
2018-09-25 01:12:07 -0300

    third commit

--- functions_onclick.js
+++ functions_onclick.js
@@ -18,7 +18,7 @@ function button_big_gateway(_this) {
 			location.href = url[1];
 	    }
 	};
-	xhttp.open("GET", "writefile.php?data=Client:4", true);
+	xhttp.open("GET", url[0], true);
 	xhttp.send();
 
 
